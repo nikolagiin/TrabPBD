a81001bbb3e6c1c4798eff12a7b2ccd6c53ee2e0
nikolagiin <nikolaiorkov@hotmail.com>
2022-11-19 23:48:10 -0300

    modificado valores do form para ser utilizado no trabalho

--- formulario.php
+++ formulario.php
@@ -24,7 +24,7 @@
         </div>
     </header>
 
-    <form>
+    <form action="advocacia.php" method="post">
         <div id="radio">
             <input type="radio" id="advo" name="selecao" value="Advogado" checked>
             <label for="selecao" >teste</label>
@@ -34,23 +34,37 @@
             <label for="selecao">réu</label>
         </div>
         
-        <label for="nome" class="elemento">Nome</label>
-        <br>
-        <input type="text" id="nome" name="nome" placeholder="Fulano">
-        <br>
-        <label for="sobrenome" class="elemento">Sobrenome</label>
-        <br>
-        <input type="text" id="sobrenome" name="sobrenome" placeholder="DeTal">
-        <br>
-        <label for="email" class="elemento">Email</label>
-        <br>
-        <input type="email" id="email" name="email" placeholder="correioeletronico@fulanodetal.com">
-        <br>
-        <label for="telefone" class="elemento">Telefone</label>
-        <br>
-        <input type="tel" id="telefone" name="telefone" placeholder="(XX) X XXXX-XXXX">
-        <br>
-        <input type="submit" value="Enviar">
+        <div id="dados">
+            <label for="oab" class="elemento">Registro OAB</label>
+            <br>
+            <input type="text" id="oab" name="oab" placeholder="UF000000A">
+            <br>
+            <label for="ramo" class="elemento">Ramo</label>
+            <br>
+            <input type="text" id="ramo" name="ramo" placeholder="Homicidio">
+            <br>
+            <label for="nome" class="elemento">Nome</label>
+            <br>
+            <input type="text" id="nome" name="nome" placeholder="Fulano">
+            <br>
+            <label for="email" class="elemento">Email</label>
+            <br>
+            <input type="email" id="email" name="email" placeholder="correioeletronico@fulanodetal.com">
+            <br>
+            <label for="telefone" class="elemento">Telefone</label>
+            <br>
+            <input type="tel" id="telefone" name="telefone" placeholder="(XX) X XXXX-XXXX">
+            <br>
+            <label for="endereco" class="elemento">Endereço</label>
+            <br>
+            <input type="text" id="endereco" name="endereco" placeholder="rua do caralho voador, 666">
+            <br>
+            <label for="cnpj" class="elemento">CNPJ</label>
+            <br>
+            <input type="text" id="cnpj" name="cnpj" placeholder="000000.0000-11">
+            <br>
+            <input type="submit" name="submit" value="enviar">
+        </div>
     </form>
 </body>
 </html>
