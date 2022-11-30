c384b9530927f443c7512ab5d49a5b6cb52f7452
nikolagiin <nikolaiorkov@hotmail.com>
2022-11-30 14:20:10 -0300

    formulário, arquivo .php e .css para pessoas

---
+++ formulario_pessoas.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscrição de Pessoas</title>
+    <link rel="stylesheet" href="style_pess.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1 id="marca">Advocacia Los Bacanos e Cia</h1>
+
+            <nav>
+                <ul>
+                    <li><a href="formulario_advogado.php">Cadastro de Advogado</a></li>
+                    <li><a href="formulario_juiz.php">Cadastro de Juiz</a></li>
+                    <li><a href="formulario_casos.php">Cadastro de Casos</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <form action="pessoas.php" method="post">
+        <div id="dados">
+            <label for="cpf" class="elemento">CPF</label>
+            <input type="text" id="cpf" name="cpf" placeholder="000.111.222-33" required>
+            <br>
+            <label for="pnome" class="elemento">Primeiro nome</label> 
+            <input type="text" id="pnome" name="pnome" placeholder="Fulano" required>
+            <br>
+            <label for="mnome" class="elemento">Sigla do nome do meio</label>
+            <input type="text" id="mnome" name="mnome" placeholder="D">
+            <br>
+            <label for="snome" class="elemento">Sobrenome</label>
+            <input type="text" id="snome" name="snome" placeholder="Tal" required>
+            <br>
+            <label for="cep" class="elemento">CEP</label>
+            <input type="text" id="cep" name="cep" placeholder="00000-111">
+            <br>
+            <label for="email" class="elemento">E-mail</label>
+            <input type="email" id="email" name="email" placeholder="correioeletronico@fulanodetal.com">
+            <br>
+            <label for="telefone" class="elemento">Telefone</label>
+            <input type="tel" id="telefone" name="telefone" placeholder="(XX) X XXXX-XXXX">
+            <br>
+            <label for="endereco" class="elemento">Endereço</label>
+            <input type="text" id="endereco" name="endereco" placeholder="rua da senhora idosa, 666">
+            <br>
+
+            <label for="tipo" class="elemento">Deseja se cadastrar como?</label>
+            <div id="radio">
+                <input type="radio" id="tipo" name="tipo" value="1">
+                <label for="autor" class="tipo">Autor</label>
+                <input type="radio" id="tipo" name="tipo" value="2">
+                <label for="reu" class="tipo">Réu</label>
+                <input type="radio" id="tipo" name="tipo" value="3">
+                <label for="juiz" class="tipo">Juiz</label>
+            </div>
+            <br>
+            <button type="submit" name="submit-pess">Enviar Formulário</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ pessoas.php
@@ -0,0 +1,73 @@
+<?php    
+    
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "advocacia";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+
+if(isset($_POST['submit-pess'])){
+
+    {
+        $cpf = $_POST['cpf'] ;
+        $pnome = $_POST['pnome'] ;
+        $mnome = $_POST['mnome'] ;
+        $snome = $_POST['snome'] ;
+        $cep = $_POST['cep'] ;
+        $email = $_POST['email'] ;
+        $telefone = $_POST['telefone'] ;
+        $endereco = $_POST['endereco'] ;
+        $tipo = $_POST['tipo'] ;
+
+        $query = "INSERT INTO pessoas(cpf, pnome, mnome, snome, cep, telefone, email, endereco) VALUES('$cpf', '$pnome', '$mnome', '$snome', '$cep', '$telefone', '$email', '$endereco')" ;
+
+        $run = mysqli_query($conn, $query) or die("Connect failed: %s\n". $conn -> error);
+
+        if($run){
+            echo "Formulário enviado CARALHO";
+        }
+        else{
+            echo "NÃO FOI PORRA VAI SE FUDEEEEE";
+        }
+    
+
+        switch($tipo){
+            case 1:
+
+                $query_autor= "INSERT INTO autor(cpfautor, pnome, mnome, snome, cep, telefone, email, endereco) VALUES('$cpf', '$pnome', '$mnome', '$snome', '$cep', '$telefone', '$email', '$endereco')" ;
+
+                $run_autor = mysqli_query($conn, $query_autor) or die("Connect failed: %s\n". $conn -> error);
+
+                if($run_autor){
+                    echo "\nTransferido para Autor";
+                }
+            break;
+            
+            case 2:
+
+                $query_reu = "INSERT INTO reu(cpfreu, pnome, mnome, snome, cep, telefone, email, endereco) VALUES('$cpf', '$pnome', '$mnome', '$snome', '$cep', '$telefone', '$email', '$endereco')" ;
+        
+
+                $run_reu = mysqli_query($conn, $query_reu) or die("Connect failed: %s\n". $conn -> error);
+
+                if($run_reu){
+                    echo "\nTransferido para Réu";
+                }
+            break;
+
+            case 3: 
+
+                $query_juiz = "INSERT INTO juiz(cpfjuiz, pnome, mnome, snome, cep, telefone, email, endereco) VALUES('$cpf', '$pnome', '$mnome', '$snome', '$cep', '$telefone', '$email', '$endereco')" ;
+
+                $run_juiz = mysqli_query($conn, $query_juiz) or die("Connect failed: %s\n". $conn -> error);
+
+                if($run_juiz){
+                    echo "\nTransferido para Juiz";
+                }
+            break;
+        }
+        
+}
+}
+?>
