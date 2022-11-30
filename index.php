074015fab574e0ebd231a69e91bce9eff43d4692
nikolagiin <nikolaiorkov@hotmail.com>
2022-11-30 14:23:14 -0300

    página inicial e .css

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial Los Bacanos</title>
+    <link rel="stylesheet" href="style_index.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
 
-include 'db_connnection.php';
-$conn = OpenCon();
-echo "Connected Successfully";
-CloseCon($conn);
+<body>
+    <div>
+        <h1 id="marca">Advocacia Los Bacanos e Cia</h1>
 
-?>
+        <nav>
+            <ul>
+                <li><a href="formulario_casos.php">Cadastro de Casos</a></li>
+                <li><a href="formulario_advogado.php">Cadastro de Advogado</a></li>
+                <li><a href="formulario_pessoas.php">Cadastro de Pessoas</a></li>
+            </ul>
+         </nav>
+    </div>
+</body>
+
+</html>
